65d436f8db970e7548c854c2273c22fb82a9a803
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-06 17:44:29 -0300

    Novo exercicio

--- estrutura_sequencial/ex06.py
+++ estrutura_sequencial/ex06.py
@@ -1,1 +1,6 @@
 #Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+pi = float(3.14)
+raio = float(input("Digite o raio do circulo em metros: "))
+calc = float(pi * (raio ** 2))
+print(f"A área é: {calc}m")
