f64a4be05f58ad9d5bdbec78b96f11c2093be4bd
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-15 12:37:53 -0300

    Ex07

---
+++ estrutura_sequencial/ex7.py
@@ -0,0 +1,7 @@
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado1 = input("Lado 1: ")
+lado2 = input("Lado 2: ")
+
+area = lado1 * lado2
+print(f"{area * 2}")
