3744fe18a286e368eaa3eb2646434c71c2a6cf5e
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-15 16:25:28 -0300

    ex08

--- estrutura_sequencial/ex08.py
+++ estrutura_sequencial/ex08.py
@@ -1,1 +1,6 @@
 #Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+ganho_por_hora = float(input("Quanto você ganha por hora? "))
+horas_trabalhadas = float(input("Quantas horas você trabalha no mês? "))
+
+calc_mes = float(ganho_por_hora * horas_trabalhadas)
+print(f"Você ganha R${calc_mes} por mês!")
