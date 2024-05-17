6ae5c250a1fe03bba897af4774ba1704e900e9b1
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-17 17:43:41 -0300

    att

--- estrutura_sequencial/ex18.py
+++ estrutura_sequencial/ex18.py
@@ -1,5 +1,7 @@
 #Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
 
+tempo = 60 #Const
+
 tamanho_arquivo = float(input("Digite o tamanho do arquivo (em MB): "))
 velocidade_link = float(input("Digite a velocidade da sua internet (em Mbps): "))
 
