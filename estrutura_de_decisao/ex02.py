29c2f57db17f4c6dae333b9f727fe4d1ce0fb5c5
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-18 13:49:29 -0300

    ex02

---
+++ estrutura_de_decisao/ex02.py
@@ -0,0 +1,10 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+valor = float(input("Digite um valor: "))
+
+if valor > 0:
+    print("Positivo")
+elif valor < 0:
+    print("Negativo")
+else:
+    print("Neutro")
