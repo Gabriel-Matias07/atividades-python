b6ea42acf90092e8fedb2ac11463c6da2b70db09
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-20 19:36:37 -0300

    ex08

--- estrutura_de_decisao/ex08.py
+++ estrutura_de_decisao/ex08.py
@@ -5,8 +5,8 @@ produto2 = float(input("Digite o preço do produto 2: "))
 produto3 = float(input("Digite o preço do produto 3: "))
 
 if produto1 < produto2 and produto1 < produto3:
-    print(f"{produto1} é o mais barato.")
+    print("O roduto 1 é o mais barato.")
 elif produto2 < produto1 and produto2 < produto3:
-    print(f"{produto2} é o mais barato.")
+    print("O produto 2 é o mais barato.")
 elif produto3 < produto1 and produto3 < produto2:
-    print(f"{produto3} é o mais barato.")
+    print("Oproduto 3 é o mais barato.")
