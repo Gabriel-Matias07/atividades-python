7c203dca2d97a8a43f0fb4e5c89d1b2e085210bb
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-23 20:09:18 -0300

    att

--- estrutura_sequencial/ex11.py
+++ estrutura_sequencial/ex11.py
@@ -3,12 +3,9 @@
 numUm = int(input("Digite o primeiro número inteiro: "))
 numDois = int(input("Digite o segundo número inteiro: "))
 numTres = float(input("Digite o terceiro número real: "))
-
 calcA = (numUm * 2) * (numDois / 2)
 print(f"A) O produto do dobro do primeiro com metade do segundo: {calcA}.")
-
 calcB = (numUm * 3) + (numTres)
 print(f"B) A soma do triplo do primeiro com o terceiro: {calcB}.")
-
 calcC = (numTres ** 3)
 print(f"C)O terceiro elevado ao cubo: {calcC}.")
