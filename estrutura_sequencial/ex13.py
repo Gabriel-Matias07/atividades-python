a71761c9bc2e1f4261eb82213c85cb808125c8a6
Gabriel Matias <gabrielpereiramatias07@gmail.com>
2024-05-23 20:09:30 -0300

    att

--- estrutura_sequencial/ex13.py
+++ estrutura_sequencial/ex13.py
@@ -3,6 +3,5 @@
 altura = float(input("Digite a sua altura em metros: "))
 peso_ideal_homens = (72.7 * altura) - 58
 peso_ideal_mulheres = (62.1 * altura) - 44.7
-
 print(f"Peso ideal para homens: {peso_ideal_homens}\n")
 print(f"Peso ideal para mulheres: {peso_ideal_mulheres}")
